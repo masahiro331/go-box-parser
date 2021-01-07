e3bd4f7f6697e180bd260f58d29ae9402f348817
masahiro331 <mur4m4s4.331@gmail.com>
2021-01-07 16:04:46 +0900

    add reader

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/masahiro331/go-box-parser
+
+go 1.15
+
+require (
+	github.com/jochasinga/requests v0.0.0-20160909035013-af9bbdb4ab2c
+	github.com/pkg/errors v0.9.1
+)
---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/masahiro331/go-box-parser/vagrantcloud"
+)
+
+func main() {
+	reader, err := vagrantcloud.GetBox("generic/alpine38", "3.1.20")
+	if err != nil {
+		log.Fatal(err)
+	}
+	treader, err := vagrantcloud.NewBoxReader(reader)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		header, err := treader.Next()
+		if err != nil {
+			break
+		}
+		fmt.Println(header.Name)
+	}
+
+}
